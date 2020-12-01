815bec1e5269a5f6768e9c53a688a8caf1368115
Calum Scott <calum.scott27@gmail.com>
2020-12-01 21:56:58 +0000

    refactored code and styled UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,128 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./app.css";
 
 function App(props: any) {
-
   const [state, setState] = useState(props.initialState);
 
-  const getPosts = (cb: any) => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((json) => {
-      const result:{id: number, title: string, body: string, userID: number}[] = [];
-      
-      json.forEach((record:{id: number, title: string, body: string, userID: number}) => result.push(record));
+  const [getPostsError, setGetPostsError] = useState<Boolean>(false);
+  const [getPostError, setGetPostError] = useState<Boolean>(false);
 
-      cb(result);
-    })
-  }
+  const parseResponse = (response: any) => {
+    try {
+      return response.json();
+    } catch (error) {
+      return null;
+    }
+  };
+
+  const getPosts = async (): Promise<any> => {
+    let result;
+
+    try {
+      result = await fetch("https://jsonplaceholder.typicode.com/posts");
+    } catch (error) {
+      return null;
+    }
+
+    if (result) {
+      return parseResponse(result);
+    } else {
+      return null;
+    }
+  };
+
+  const getPost = async (postId: number): Promise<any> => {
+    let result;
+
+    try {
+      result = await fetch(
+        "https://jsonplaceholder.typicode.com/posts/" + postId
+      );
+    } catch (error) {
+      return null;
+    }
+
+    if (result) {
+      return parseResponse(result);
+    } else {
+      return null;
+    }
+  };
+
+  const handlePostsClick = async () => {
+    const posts: {
+      id: number;
+      title: string;
+      body: string;
+      userID: number;
+    }[] = await getPosts();
+
+    if (posts) {
+      setState({ ...state, posts: posts });
+    } else {
+      setGetPostsError(true);
+    }
+  };
+
+  const handleViewClick = async (postId: number) => {
+    const post: {
+      id: number;
+      title: string;
+      body: string;
+      userID: number;
+    } = await getPost(postId);
+
+    if (post) {
+      setState({ ...state, view: post });
+    } else {
+      setGetPostError(true);
+    }
+  };
 
   return (
     <div className="App">
-      <h1 className="heading">
-        {state.heading}
-      </h1>
+      <div className="header">
+        <h1 className="heading">{state.heading}</h1>
+        <button className="load-posts" onClick={() => handlePostsClick()}>
+          Fetch posts
+        </button>
+      </div>
       <div className="content">
-        <button className="load-posts" onClick={() => getPosts((values: any) => {
-          var update = {
-            posts: state.posts,
-            heading: state.heading,
-            view: state.view,
-          };
-
-          update.posts = values;
-
-          setState(update);
-        })}>Fetch posts</button>
-        <ul className="posts">
-          {state.posts.map((post: any, index: number) => {
-            return (<div role="listitem" className="post-card" key={index}>
-              <h2 className="post-title">
-                {post.title}
-              </h2>
-              <button className="show-post" onClick={() => {
-                  fetch('https://jsonplaceholder.typicode.com/posts/' + post.id)
-                  .then(response => response.json())
-                  .then(json => {
-                    var updateView = {
-                      posts: state.posts,
-                      heading: state.heading,
-                      view: state.view,
-                    };
-
-                    updateView.view = json;
-                    
-                    setState(updateView);
-                  })
-              }}>View</button>
-            </div>)
-          })}
-        </ul>
-        { state.view && 
-        <article className="viewer">
-          <h2 className="viewer-title">{state.view.title}</h2>
-          <p className="viewer-content">
-            {state.view.body}
-          </p>
-        </article>}
+        <div>
+          <div className="content-container">
+            <div>
+              {getPostsError && <h1> There was an issue getting posts</h1>}
+              {state.posts && (
+                <ul className="posts">
+                  {state.posts.map((post: any, index: number) => {
+                    return (
+                      <div role="listitem" className="post-card" key={index}>
+                        <h2 className="post-title">{post.title}</h2>
+                        <button
+                          className="show-post"
+                          onClick={() => {
+                            handleViewClick(post.id);
+                          }}
+                        >
+                          View
+                        </button>
+                      </div>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+            <div className="post-container">
+              {getPostError && <h1> There was an issue getting this post.</h1>}
+              {state.view && (
+                <article className="viewer">
+                  <h2 className="viewer-title">{state.view.title}</h2>
+                  <p className="viewer-content">{state.view.body}</p>
+                </article>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const initialValues = {
-  heading: 'Present Pal',
+  heading: "Present Pal",
   posts: [],
   view: null,
-}
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App initialState={initialValues} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
